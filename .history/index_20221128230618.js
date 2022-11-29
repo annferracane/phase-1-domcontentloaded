fdb142e7cc6587e47b80c104746313888a7cca9a
Ann Ferracane <ann@patchventures.com>
2022-11-28 23:06:48 -0500

    commit tree 1f8feaa651d6f412b0a2f3c02537f09a8287902d

---
+++ .history/index_20221128230618.js
@@ -0,0 +1,9 @@
+// Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.querySelector("p").textContent = "This is really cool!"
+  });
+
+  document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+  
--- .history/index_20221128230630.js
+++ .history/index_20221128230630.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("p").textContent = "This is really cool!"
-});
+  });
 
 document.querySelector("p").textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
   
